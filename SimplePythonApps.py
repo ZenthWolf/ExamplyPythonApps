3cbe9ef40edeb66003297f017b3a09eb909368ac
ZenthWolf <ZenthRelin@gmail.com>
2023-10-05 11:35:27 -0400

    Scraping HTML from URL

--- SimplePythonApps.py
+++ SimplePythonApps.py
@@ -6,4 +6,17 @@ Created on Thu Oct  5 09:55:12 2023
 @author: zenth
 """
 
-#%% Cell
+#%% Cell Imports
+
+import requests
+
+#%% Scrape WisdomPetMedicine (fictional business)
+
+response = requests.get('https://wisdompetmed.com')
+
+response.url           # Check URL is correct
+response.status_code   # Check website Status is good 2xx
+response.headers       # Check headers
+response.content       # HTML in byte data
+response.text          # HTML as UTF-8 string (More readable, might less accurate)
+
